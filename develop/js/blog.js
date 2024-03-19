8c8cd1c9fddb6947d6a0a615fd6e204bc3b872d0
Swemee <sweme1@gmail.com>
2024-03-18 21:25:36 -0600

    Added full functionality to the website

--- develop/js/blog.js
+++ develop/js/blog.js
@@ -1,17 +1,47 @@
+const blogPosts = document.getElementById('container');
+const backBut = document.getElementById('backButton');
 
+const header = document.querySelector('header');
 
 
 function renderBlogs() {
-    let blogInfo = JSON.parse(localStorage.getItem('blog'));
-    
+    let blogInfo = JSON.parse(localStorage.getItem('blog')) || [];
 
-    for (let i = 0; i < blogs.length; i++) {
-        const posts = blogs[i];
+    for (let i = 0; i < blogInfo.length; i++) {
+        const posts = blogInfo[i];
+        
 
-        const li = document.createElement('li');
-        li.textContent = posts.username;
-        ulPosts.appendChild(li);
+        const divider = document.createElement('div');
+        divider.className = 'wrapper';
+
+        const userText = document.createElement('h2');
+        userText.textContent = blogInfo[i].username;
+        userText.className = 'userTextContent';
+
+        const titleText = document.createElement('h3');
+        titleText.textContent = blogInfo[i].title;
+        titleText.className = 'userTitleContent';
+
+        const contentText = document.createElement('p');
+        contentText.textContent = blogInfo[i].content;
+        contentText.className = 'userContentContent';
+
+        blogPosts.appendChild(divider);
+
+        divider.appendChild(userText);
+        divider.appendChild(titleText);
+        divider.appendChild(contentText);
     };
 
 }
+
+backBut.addEventListener('click', function(event) {
+    window.location.assign('./index.html');
+})
+
+function init() {
+    renderBlogs();
+}
+init();
+
 
--- develop/js/form.js
+++ develop/js/form.js
@@ -1,13 +1,12 @@
-const formPage = document.querySelector("#blogForm")
 const subBut = document.querySelector("#submitButton");
 const usernameInput = document.getElementById("username");
 const titleInput = document.getElementById("title");
 const contentInput = document.getElementById("content");
-const ulPosts = document.getElementById("posts");
 
 
 // This function creates and renders the blog
 function createAndRenderBlog() {
+    
     const blog = {
         username: usernameInput.value,
         title: titleInput.value,
@@ -16,15 +15,27 @@ function createAndRenderBlog() {
 
     let blogs = JSON.parse(localStorage.getItem('blog')) || [];
     blogs.push(blog);
-    
     localStorage.setItem('blog', JSON.stringify(blogs));
-    
+  
+
 }
 
+//Make if statement for if they complete the form or not
+function incompleteForm() {
+    window.alert('Please complete the form.');
+}
+
+
+//This function makes it so that if the form is complete then move on to the blog page, if not then an alert pops up
 subBut.addEventListener('click', function(event) {
-    event.preventDefault();
-    createAndRenderBlog();
-    window.location.assign('./blogpage.html');
+
+    if (usernameInput.value === '' || titleInput.value === '' || contentInput.value === '') {
+        incompleteForm();
+    } else {
+        event.preventDefault();
+        createAndRenderBlog();
+        window.location.assign('./blogpage.html');
+    }
 })
 
 
